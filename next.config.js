9ef4c3a1aa236944b9c890c40386016903cb660b
Alex Klos <alex@prohobo.dev>
2026-07-02 11:30:39 +0200

    ignore sharp in build

--- next.config.js
+++ next.config.js
@@ -4,22 +4,6 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    turbopack: {
-        rules: {
-            '*.glsl': {
-                loaders: ['raw-loader'],
-                as: '*.js',
-            },
-        },
-    },
-    webpack: (config, {}) => {
-        config.module.rules.push({
-            test: /\.glsl/,
-            type: "asset/source",
-        });
-
-        return config;
-    },
 };
 
 module.exports = nextConfig;
